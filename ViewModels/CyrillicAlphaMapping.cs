c69e69934e49fb1a467986ff274724bd1b1488ba
wesdoyle <wesdoyle08@gmail.com>
2020-06-23 11:53:10 -0500

    Add sample questions,

---
+++ ViewModels/CyrillicAlphaMapping.cs
@@ -0,0 +1,5 @@
+namespace LangLearn.ViewModels {
+    public class CyrillicAlphaMapping {
+        
+    }
+}
---
+++ ViewModels/VocabQuestion.cs
@@ -0,0 +1,9 @@
+namespace LangLearn.ViewModels {
+    /// <summary>
+    /// Maps Cyrillic Letters to English Phoneme
+    /// </summary>
+    public class CyrillicAlphaMapping {
+        public string Cyrillic { get; set; }
+        public string Phoneme { get; set; }
+    }
+}
